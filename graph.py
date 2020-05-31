2544b3608d758aee33e73e3d9de817f1dc942a90
Caesar-8C <kosenko941m41@gmail.com>
2020-05-31 22:51:56 +0200

    added scaling(works), shifting(doesn't work)

--- graph.py
+++ graph.py
@@ -4,6 +4,28 @@ import networkx as nx
 import sys
 import time
 
+class Draw:
+	def __init__(self, scale=1.5, shift=(0, 0)):
+		self.scale = scale
+		self.shift = shift
+
+	def line(self, surface, color, start_pos, end_pos, width):
+		start_pos = (int(start_pos[0]*self.scale + self.shift[0]), int(start_pos[1]*self.scale + self.shift[1]))
+		end_pos = (int(end_pos[0]*self.scale + self.shift[0]), int(end_pos[1]*self.scale + self.shift[1]))
+		pyg.draw.line(surface, color, start_pos, end_pos, width)
+
+	def circle(self, surface, color, center, radius):
+		center = (int(center[0]*self.scale + self.shift[0]), int(center[1]*self.scale + self.shift[1]))
+		radius = int(radius*self.scale)
+		pyg.draw.circle(surface, color, center, radius)
+
+	def polygon(self, surface, color, points):
+		p = []
+		for point in points:
+			p.append((int(point[0]*self.scale + self.shift[0]), int(point[1]*self.scale + self.shift[1])))
+		pyg.draw.polygon(surface, color, tuple(p))
+
+
 class Map:
 	def __init__(self):
 		self.G = nx.DiGraph()
@@ -15,9 +37,15 @@ class Map:
 		self.agents = {}
 		self.fps = 60
 		self.agentNameCounter = 0
+		self.draw = Draw()
 
 		self.activeNode = False
 		self.activeEdge = False
+		self.activeBackground = False
+
+		self.NODE_SIZE = 10
+		self.MIN_CLICKABLE_CORRIDOR_WIDTH = 5
+		self.LANE_PIXEL_WIDTH = 20
 
 		self.AGENT1 = pyg.USEREVENT+1
 		self.AGENT2 = pyg.USEREVENT+2
@@ -37,7 +65,7 @@ class Map:
 	def run_agent(self, origin_node, end_node, lane, speed):
 		agent = self.Agent(origin_node, end_node, lane, speed)
 		self.agents[self.agentNameCounter] = agent
-		self.agentNameCounter += 1 
+		self.agentNameCounter += 1
 
 	def add_node(self, name, x, y):
 		self.G.add_node(name)
@@ -50,8 +78,19 @@ class Map:
 		self.G.edges[start, end]['laneWidth'] = width
 		self.G.edges[start, end]['rooms'] = {}
 
+	def activeReset(self):
+		self.activeNode = False
+		self.activeEdge = False
+		self.activeBackground = False
+
 	def main(self):
+		def tupleSum(t1, t2):
+			return (t1[0] + t2[0], t1[1] + t2[1])
+		def tupleSubtract(t1, t2):
+			return (t1[0] - t2[0], t1[1] - t2[1])
+
 		def activateElement(pos):
+			pos = (int(pos[0]/self.draw.scale), int(pos[1]/self.draw.scale)) #TODO add shift to mouse
 			def between(a, b, c):
 				if b > c:
 					if b > a and a > c:
@@ -62,8 +101,8 @@ class Map:
 				return False
 
 			for node in list(self.G.nodes):
-				dist = np.linalg.norm(np.subtract(self.G.nodes[node]['coordinates'], pos))
-				if dist < self.G.degree[node]*10:
+				dist = np.linalg.norm(tupleSubtract(self.G.nodes[node]['coordinates'], pos))
+				if dist < self.NODE_SIZE:
 					self.activeNode = node
 					return
 
@@ -80,12 +119,13 @@ class Map:
 				y = int((a*(a*y0 - b*x0)-b*c)/(a**2 + b**2))
 				if between(x, x1, x2) and between(y, y1, y2):
 					dist = np.abs((y2-y1)*x0 - (x2-x1)*y0 + x2*y1 - y2*x1)/np.sqrt((y2-y1)**2 + (x2-x1)**2)
-					if dist < self.G.edges[edge]['width']/2 or dist < 5:
+					if dist < self.G.edges[edge]['width']/2 or dist < self.MIN_CLICKABLE_CORRIDOR_WIDTH:
 						self.activeEdge = edge
 						return
+			self.activeBackground = True
 
-		def tupleSum(t1, t2):
-			return (t1[0] + t2[0], t1[1] + t2[1])
+		def mouseScale(rel):
+			return (int(rel[0]/self.draw.scale), int(rel[1]/self.draw.scale))
 
 		def eventHandlerLoop():
 			for event in pyg.event.get():
@@ -94,28 +134,34 @@ class Map:
 				if event.type == pyg.MOUSEBUTTONDOWN and event.button == 1:
 					activateElement(event.pos)
 				if event.type == pyg.MOUSEBUTTONUP and event.button == 1:
-					self.activeNode = False
-					self.activeEdge = False
+					self.activeReset()
 				if event.type == pyg.MOUSEMOTION and not self.activeNode == False:
-					self.G.nodes[self.activeNode]['coordinates'] = tupleSum(self.G.nodes[self.activeNode]['coordinates'], event.rel)
+					self.G.nodes[self.activeNode]['coordinates'] = tupleSum(self.G.nodes[self.activeNode]['coordinates'], mouseScale(event.rel))
 				if event.type == pyg.MOUSEMOTION and not self.activeEdge == False:
-					self.G.nodes[self.activeEdge[0]]['coordinates'] = tupleSum(self.G.nodes[self.activeEdge[0]]['coordinates'], event.rel)
-					self.G.nodes[self.activeEdge[1]]['coordinates'] = tupleSum(self.G.nodes[self.activeEdge[1]]['coordinates'], event.rel)
+					self.G.nodes[self.activeEdge[0]]['coordinates'] = tupleSum(self.G.nodes[self.activeEdge[0]]['coordinates'], mouseScale(event.rel))
+					self.G.nodes[self.activeEdge[1]]['coordinates'] = tupleSum(self.G.nodes[self.activeEdge[1]]['coordinates'], mouseScale(event.rel))
+
+
+				if event.type == pyg.MOUSEMOTION and self.activeBackground == True:
+					self.draw.shift = tupleSum(self.draw.shift, mouseScale(event.rel))
 
 				if event.type == pyg.MOUSEBUTTONDOWN and event.button == 4:
 					activateElement(event.pos)
 					if not self.activeEdge == False:
 						self.G.edges[self.activeEdge]['width'] += 4
-					self.activeNode = False
-					self.activeEdge = False
+					if self.activeBackground == True:
+						self.draw.scale *= 1.1
+					self.activeReset()
 				if event.type == pyg.MOUSEBUTTONDOWN and event.button == 5:
 					activateElement(event.pos)
 					if not self.activeEdge == False:
 						self.G.edges[self.activeEdge]['width'] -= 4
 						if self.G.edges[self.activeEdge]['width'] < 1:
 							self.G.edges[self.activeEdge]['width'] = 1
-					self.activeNode = False
-					self.activeEdge = False
+					if self.activeBackground == True:
+						self.draw.scale /= 1.1
+					self.activeReset()
+
 				if event.type == self.AGENT1:
 					self.run_agent(origin_node=2, end_node=3, lane=1, speed=60)
 				if event.type == self.AGENT2:
@@ -128,22 +174,22 @@ class Map:
 			y1 = self.G.nodes[node]['coordinates'][1]
 			x2 = self.G.nodes[successor]['coordinates'][0]
 			y2 = self.G.nodes[successor]['coordinates'][1]
+			halfWidth = self.G.edges[node, successor]['width']/2
 
 			angle = np.arctan2(x1-x2, y2-y1)
-			width = self.G.edges[node, successor]['width']/2
-			x3 = x1 + np.cos(angle)*width
-			y3 = y1 + np.sin(angle)*width
+			x3 = x1 + np.cos(angle)*halfWidth
+			y3 = y1 + np.sin(angle)*halfWidth
 
-			x4 = x1 - np.cos(angle)*width
-			y4 = y1 - np.sin(angle)*width
+			x4 = x1 - np.cos(angle)*halfWidth
+			y4 = y1 - np.sin(angle)*halfWidth
 
-			x5 = x2 - np.cos(angle)*width
-			y5 = y2 - np.sin(angle)*width
+			x5 = x2 - np.cos(angle)*halfWidth
+			y5 = y2 - np.sin(angle)*halfWidth
 
-			x6 = x2 + np.cos(angle)*width
-			y6 = y2 + np.sin(angle)*width
+			x6 = x2 + np.cos(angle)*halfWidth
+			y6 = y2 + np.sin(angle)*halfWidth
 
-			pyg.draw.polygon(self.screen, color, ((x3, y3), (x4, y4), (x5, y5), (x6, y6)))
+			self.draw.polygon(self.screen, color, ((x3, y3), (x4, y4), (x5, y5), (x6, y6)))
 		def drawRooms(color, node, successor):
 			x1 = self.G.nodes[node]['coordinates'][0]
 			y1 = self.G.nodes[node]['coordinates'][1]
@@ -161,7 +207,7 @@ class Map:
 				x4 = x3 + np.cos(angleAlong)*20
 				y4 = y3 + np.sin(angleAlong)*20
 
-				pyg.draw.line(self.screen, color, (x3, y3), (x4, y4), 3)
+				self.draw.line(self.screen, color, (x3, y3), (x4, y4), 3)
 		def drawLanes(color, node, successor):
 			x1 = self.G.nodes[node]['coordinates'][0]
 			y1 = self.G.nodes[node]['coordinates'][1]
@@ -169,7 +215,7 @@ class Map:
 			y2 = self.G.nodes[successor]['coordinates'][1]
 			width = self.G.edges[node, successor]['width']
 			
-			lanes = int(width/20)
+			lanes = int(width/self.LANE_PIXEL_WIDTH)
 			if lanes == 0:
 				lanes = 1
 			self.G.edges[node, successor]['lanes'] = lanes
@@ -197,7 +243,7 @@ class Map:
 					x4 -= xStep
 					y4 -= yStep
 
-					pyg.draw.line(self.screen, color, (x3, y3), (x4, y4), 1)
+					self.draw.line(self.screen, color, (x3, y3), (x4, y4), 1)
 		def drawAgents(color):
 			indicesToDelete = []
 			for agentIndex in self.agents:
@@ -224,7 +270,7 @@ class Map:
 					x3 += x2
 					y3 += y2
 
-				pyg.draw.circle(self.screen, color, (x3, y3), int(laneWidth/2))
+				self.draw.circle(self.screen, color, (x3, y3), int(laneWidth/2))
 
 				agent.distance += agent.speed/self.fps
 
@@ -246,12 +292,7 @@ class Map:
 				if successor not in visited:
 					drawSuccessors(successor, visited)
 
-			# rad = 0
-			# for edge in self.G.edges:
-			# 	if node in edge and self.G.edges[edge]['width'] > rad:
-			# 		rad = self.G.edges[edge]['width']
-			# pyg.draw.circle(self.screen, (128, 128, 0), self.G.nodes[node]['coordinates'], int(rad/2.))
-			pyg.draw.circle(self.screen, (0, 128, 0), self.G.nodes[node]['coordinates'], self.G.degree[node]*10)
+			self.draw.circle(self.screen, (0, 128, 0), self.G.nodes[node]['coordinates'], self.NODE_SIZE)
 
 		while True:
 			self.screen.fill(self.bgcolour)
@@ -264,6 +305,7 @@ class Map:
 			drawAgents((0, 0, 100))
 
 			pyg.display.flip()
+
 
 if __name__ == '__main__':
 	map = Map()
